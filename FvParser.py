33d296cdf988303752baebd0028eae542ff054a6
ren.yang <goyour123@gmail.com>
2019-04-14 00:42:20 +0800

    Add EFI_SECTION_GUID_DEFINED parsing

--- FvParser.py
+++ FvParser.py
@@ -85,20 +85,27 @@ if __name__ == '__main__':
           if RawBytes2Hex(ffsFileAttr) & 0x01:
             # EFI_FFS_FILE_HEADER2
             ffsExtendedSize = f.read(8)
-            fvDict['Fv'+str(fvCnt)].update({'Ffs': {'ExtendedSize': RawBytes2Readable(ffsExtendedSize)}})
+            fvDict['Fv'+str(fvCnt)]['Ffs'].update({'ExtendedSize': RawBytes2Readable(ffsExtendedSize)})
           else:
             # EFI_FFS_FILE_HEADER
             pass
 
-        # Parse EFI_COMMON_SECTION_HEADER
-        sectSize, sectType = f.read(3), f.read(1)
-        fvDict['Fv'+str(fvCnt)].update({'Ffs': {'sectSize': sectSize[::-1].hex(), \
-                                                'sectType': sectType.hex()}})
-        
-        if RawBytes2Hex(sectSize) == 0xffffff:
-          #EFI_COMMON_SECTION_HEADER2
-          sectExtSize = f.read(4)
-          fvDict['Fv'+str(fvCnt)].update({'Ffs': {'sectExtSize': sectExtSize[::-1].hex()}})
+          # Parse EFI_COMMON_SECTION_HEADER
+          sectSize, sectType = f.read(3), f.read(1)
+          fvDict['Fv'+str(fvCnt)]['Ffs'].update({'Section': {'SectSize': sectSize[::-1].hex(), \
+                                                             'SectType': sectType.hex()}})
+
+          if RawBytes2Hex(sectSize) == 0xffffff:
+            #EFI_COMMON_SECTION_HEADER2
+            sectExtSize = f.read(4)
+            fvDict['Fv'+str(fvCnt)]['Ffs']['Section'].update({'SectExtSize': sectExtSize[::-1].hex()})
+
+          if RawBytes2Hex(sectType) == 0x2:
+            #EFI_SECTION_GUID_DEFINED
+            sectDefGuid, dataOffset, sgdAttr = f.read(16), f.read(2), f.read(2)
+            fvDict['Fv'+str(fvCnt)]['Ffs']['Section'].update({'SectionDefinitionGuid': str(RawGuid2Uuid(sectDefGuid)), \
+                                                              'DataOffset': dataOffset[::-1].hex(), \
+                                                              'Attributes': sgdAttr[::-1].hex()})
 
         # Save FVs to file
         try:
